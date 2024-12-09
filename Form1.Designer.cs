40053b04642e8660e785647b539225697fffd310
jumel <alexis.jumel1@gmail.com>
2024-12-09 15:25:16 +0100

    Add Background animation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,23 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.MoveBackgroundTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MoveBackgroundTimer
+            // 
+            this.MoveBackgroundTimer.Enabled = true;
+            this.MoveBackgroundTimer.Interval = 10;
+            this.MoveBackgroundTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Space_Shooter.Properties.Resources.SpaceShip;
+            this.pictureBox1.Location = new System.Drawing.Point(245, 374);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.pictureBox1);
             this.MaximumSize = new System.Drawing.Size(600, 500);
             this.MinimumSize = new System.Drawing.Size(600, 500);
             this.Name = "Form1";
             this.Text = "Space Shooter";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer MoveBackgroundTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
